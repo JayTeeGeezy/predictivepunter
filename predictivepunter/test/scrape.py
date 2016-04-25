1824bddf6dc6a4cca061ba97c7e11124acf4d36f
Jason Green <JayTeeGeezy@outlook.com>
2016-04-26 09:00:53 +1000

    Refactor scrape into CommandLineProcessor and ScrapeProcessor

--- predictivepunter/test/scrape.py
+++ predictivepunter/test/scrape.py
@@ -1,35 +1,41 @@
 from datetime import datetime
+import logging
 import os
 import shutil
 import unittest
 
 import cache_requests
 from lxml import html
-from predictivepunter.scrape import scrape
+from predictivepunter.scrape import ScrapeProcessor
 import pymongo
 import pypunters
 
 
-class ScrapeTest(unittest.TestCase):
+class ScrapeProcessorTest(unittest.TestCase):
 
 	def test_scrape(self):
 		"""The scrape method should populate the database"""
 
-		date_from = date_to = datetime(2016, 2, 1)
+		configuration = {
+			'backup_database':	True,
+			'cache_expiry':		60 * 10,	# 10 minutes
+			'database_name':	'predictivepunter_test',
+			'date_from':		datetime(2016, 2, 1),
+			'date_to':			datetime(2016, 2, 1),
+			'logging_level':	logging.DEBUG,
+			'threads':			4
+		}
 
-		database = pymongo.MongoClient()['predictivepunter_test']
+		database = pymongo.MongoClient()[configuration['database_name']]
 		collections = ('meets', 'races', 'runners', 'horses', 'jockeys', 'trainers', 'performances')
 		for collection in collections:
 			database[collection].delete_many({})
-		dump_directory = 'dump/{name}'.format(name=database.name)
+		dump_directory = 'dump/{name}'.format(name=configuration['database_name'])
 		if os.path.isdir(dump_directory):
 			shutil.rmtree(dump_directory)
 
-		http_client = cache_requests.Session()
-		html_parser = html.fromstring
-		scraper = pypunters.Scraper(http_client, html_parser)
-
-		scrape(date_from, date_to, database, scraper, backup_database=True, threads=4)
+		processor = ScrapeProcessor(**configuration)
+		processor.process_dates(configuration['date_from'], configuration['date_to'])
 
 		for collection in collections:
 			self.assertGreater(database[collection].count(), 0)
