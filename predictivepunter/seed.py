64c33f422c0ad8c570d0cf7a7637b080984bf85c
Jason Green <JayTeeGeezy@outlook.com>
2016-04-28 09:51:55 +1000

    Save Seed.normalized_data to database

--- predictivepunter/seed.py
+++ predictivepunter/seed.py
@@ -75,8 +75,9 @@ class Seed(pyracing.Entity):
 	def normalized_data(self):
 		"""Return an array of the raw data values normalized for all runners in the race"""
 
-		if not 'normalized_data' in self.cache:
-			self.cache['normalized_data'] = [0.5 for index in range(len(self['raw_data']))]
+		if not 'normalized_data' in self:
+			
+			self['normalized_data'] = [0.5 for item in self['raw_data']]
 
 			all_seeds = [Seed.get_seed_by_runner(runner) for runner in self.runner.race.runners]
 			for index in range(len(self['raw_data'])):
@@ -85,11 +86,13 @@ class Seed(pyracing.Entity):
 					own_value = self['raw_data'][index]
 					if own_value is None:
 						average_value = sum(all_values) / len(all_values)
-						self.cache['normalized_data'][index] = (average_value - min(all_values)) / (max(all_values) - min(all_values))
+						self['normalized_data'][index] = (average_value - min(all_values)) / (max(all_values) - min(all_values))
 					else:
-						self.cache['normalized_data'][index] = (own_value - min(all_values)) / (max(all_values) - min(all_values))
+						self['normalized_data'][index] = (own_value - min(all_values)) / (max(all_values) - min(all_values))
 
-		return self.cache['normalized_data']
+			self.save()
+
+		return self['normalized_data']
 
 	@property
 	def runner(self):
